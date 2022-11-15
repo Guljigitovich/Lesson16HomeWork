fd5b3d73c76c81a4427d42d671c4fb452f74f60c
Saparov Syimyk <guljigitovich.180698@gmai.com>
2022-11-15 14:02:51 +0600

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = new Animal[]{
-                new Shark("Shark", "White", 12),
-                new Shark("Shark1", "Blue", 13),
-                new Turtle("Turtle", "Black", 11),
-                new Turtle("Turtle1", "Brown", 15),
-                new Eagle("Eagle", "Orange", 14),
-                new Eagle("Eagle1", "Black", 10)
-        };
-        Shark[] sharks = new Shark[2];
-        Eagle[] eagles = new Eagle[2];
-        Turtle[] turtles = new Turtle[2];
-        
+
+               Shark shark = new Shark("Shark", "White", 12);
+               Shark shark1= new Shark("Shark1", "Blue", 13);
+               Turtle turtle= new Turtle("Turtle", "Black", 11);
+               Turtle turtle1= new Turtle("Turtle1", "Brown", 15);
+               Eagle eagle= new Eagle("Eagle", "Orange", 14);
+               Eagle eagle1= new Eagle("Eagle1", "Black", 10);
+
+        Animal [] animals = {shark,shark1,turtle1,turtle,eagle,eagle1};
+        Shark[] sharks = {shark,shark1};
+        Eagle[] eagles = {eagle,eagle1};
+        Turtle[] turtles = {turtle,turtle1};
+
+        System.out.println(Arrays.toString(sharks));
+        System.out.println(Arrays.toString(eagles));
+        System.out.println(Arrays.toString(turtles));
+
 
         for (Animal massive : animals) {
             System.out.println(massive);
